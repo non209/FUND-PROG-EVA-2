fa84f3ac2a4c870fad1c130431c453a481acf1a2
non209 <143562722+non209@users.noreply.github.com>
2023-11-01 17:52:25 -0600

    Add files via upload

---
+++ EVA2_21_DO_WHILE/src/eva2_21_do_while/EVA2_21_DO_WHILE.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_21_do_while;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_21_DO_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor;
+        Scanner sc = new Scanner (System.in);
+        do{
+            System.out.println("Introduce un valor: ");
+        valor= sc.nextInt();
+        }while (valor !=100);
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
