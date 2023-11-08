7406ad9a3a726a3b99cc0e0ccfc0fa70900e45fb
non209 <143562722+non209@users.noreply.github.com>
2023-11-08 17:54:53 -0600

    Add files via upload

---
+++ EVA2_27_ARREGLOS/src/eva2_27_arreglos/EVA2_27_ARREGLOS.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_27_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_27_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] arregloEnteros = new int [10];
+        
+        arregloEnteros [0] = 100;
+        arregloEnteros [1] = 200;
+        arregloEnteros [2] = 300;
+        arregloEnteros [3] = 400;
+        arregloEnteros [4] = 500;
+        arregloEnteros [5] = 600;
+        arregloEnteros [6] = 700;
+        arregloEnteros [7] = 800;
+        arregloEnteros [8] = 900;
+        arregloEnteros [9] = 1000;
+        
+        System.out.println("valor en posicion 0: " + arregloEnteros [0]);
+                
+        String [] arregloCade = new String [5];
+        
+        arregloCade[0] = "Hola";
+        arregloCade[1] = " ";
+        arregloCade[2] = "mundo";
+        arregloCade[3] = " ";
+        arregloCade[4] = " cruel";
+        
+        int [] arreglo = {5,4,3,2,1};
+        System.out.println("Posicion arreglo[3:" + arreglo [3]);
+    }
+    
+}
